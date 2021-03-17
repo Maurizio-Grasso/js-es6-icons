5c258866ff92a52fb876dfb4388dd748e153ae02
maurizio <maurizio.grasso@gmail.com>
2021-03-17 21:16:51 +0100

    Milestone 3

--- main.js
+++ main.js
@@ -97,11 +97,47 @@ iconsAll = [
 	}
 ];
 
-const iconsContainer = document.getElementById('icons-container');
+const iconsContainer = document.getElementById('icons-container');	//	ul contenente tutte le icone
+const select = document.getElementById('type-select');				//	menu a tendina
+select.onchange = function(){batchPrint(this.value)};  				//	assegna evento onchange alla select
 
 assignColor(getUniqueTypes(iconsAll));	//	Definisce proprietà 'color' di ogni oggetto
+batchPrint('all');	//	Stampa tutte le icone
 
-iconsAll.forEach(element => {	printIcon(element);	});	//	Stampa tutte le cards nell'html
+
+function batchPrint(type) {
+//	in base valore passato come argomento, comanda la stampa di tutti
+//	gli elementi che rispondono al tipo corrispondente		
+	iconsContainer.innerHTML = '';
+	if(type == 'all') {
+		iconsAll.forEach( (element) => {
+			printIcon(element);
+		});
+	}
+	else {
+		iconsAll.filter((element , index) => {
+			if(element.type == type) printIcon(element);
+		});
+	}
+}
+
+function printIcon(icon) {	
+//	Stampa il box-icona passato come argomento all'interno dell'HTML
+	iconsContainer.innerHTML += `<li class="single-icon-outer"><i style="color: rgb(${icon.color});" class="${icon.family} ${icon.prefix}${icon.name}"></i><h3 class="margin-t-standard text-uppercase">${icon.name}</h3></li>`;	
+}
+
+
+function getUniqueTypes(array) {
+	//	Restituisce un array contenente tutti i diversi tipi di elemento 
+	// 	presenti nell'array passato come argomento
+	let types = [];
+	array.forEach(	(element)	=> {
+		if(!types.includes(element.type)) {
+			types.push(element.type);
+		}
+	});
+	return types;
+}
 
 function assignColor(tipiDiIcona) {
 //	Assegna una nuova proprietà 'color' ad ogni oggetto dell'array	
@@ -111,23 +147,6 @@ function assignColor(tipiDiIcona) {
 		oggetto.color = colori[tipiDiIcona.indexOf(oggetto.type)];
 		return oggetto;
 	  }); 
-}
-
-function printIcon(icon) {	
-//	Stampa il box-icona passato come argomento all'interno dell'HTML
-	iconsContainer.innerHTML += `<li class="single-icon-outer"><i style="color: rgb(${icon.color});" class="${icon.family} ${icon.prefix}${icon.name}"></i><h3 class="margin-t-standard text-uppercase">${icon.name}</h3></li>`;	
-}
-
-function getUniqueTypes(array) {
-//	Restituisce un array contenente tutti i diversi tipi di elemento 
-// 	presenti nell'array passato come argomento
-	let types = [];
-	array.forEach(	(element)	=> {
-		if(!types.includes(element.type)) {
-			types.push(element.type);
-		}
-	});
-	return types;
 }
 
 function generateColors(colors) {
