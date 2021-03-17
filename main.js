6dc61c19c48272fa9d20e3ce69dce043d1aa4117
maurizio <maurizio.grasso@gmail.com>
2021-03-17 20:17:07 +0100

    Milestone 2
    (da migliorare)

--- main.js
+++ main.js
@@ -99,8 +99,56 @@ iconsAll = [
 
 const iconsContainer = document.getElementById('icons-container');
 
+assignColor(getUniqueTypes(iconsAll));
+
+
+function assignColor(tipiPresenti) {
+	let colori = generateColors(tipiPresenti.length);
+	iconsAll = iconsAll.map( (oggetto) => { 
+		oggetto.color = colori[tipiPresenti.indexOf(oggetto.type)];
+		return oggetto; 
+	  }); 
+}
+
 iconsAll.forEach(element => {	printIcon(element);	});
 
+
 function printIcon(icon) {	
-	iconsContainer.innerHTML += `<li class="single-icon-outer"><i class="${icon.family} ${icon.prefix}${icon.name}"></i><h3 class="margin-t-standard text-uppercase">${icon.name}</h3></li>`;	
+//	Stampa un determinato box-icona all'interno dell'HTML
+	iconsContainer.innerHTML += `<li class="single-icon-outer"><i style="color: rgb(${icon.color});" class="${icon.family} ${icon.prefix}${icon.name}"></i><h3 class="margin-t-standard text-uppercase">${icon.name}</h3></li>`;	
+}
+
+function getUniqueTypes(array) {
+//	Determina i diversi tipi di elemento presenti all'interno di un array
+// 	e li restituisce come elementi di un array
+	let types = [];
+	array.forEach(	(element)	=> {
+		if(!types.includes(element.type)) {
+			types.push(element.type);
+		}
+	});
+	return types;
+}
+
+function generateColors(colors) {
+//	In base al valore passato genera tot valori RGB
+	var randomColors = [];
+	for(let i = 0; i < colors; i++) {
+		randomColors.push(getRGB());
+	}
+	return randomColors;
+}
+
+function getRGB() {
+//	Genera un valore RGB
+	let rgbColor = [];
+	for(let j = 0; j < 3; j++) {
+		rgbColor.push(getRandom(0,255));
+	}
+	return rgbColor;
+}
+
+function getRandom(min , max) {	
+//	Genera un numero casuale compreso fra min e max	
+	return Math.floor(Math.random()* ( max - min + 1 ) + min );	
 }
